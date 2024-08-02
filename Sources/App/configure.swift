0e469573b0d4109868c3977f2ed3b3be18b7bb47
Sam Clemente <sam@theclementes.com>
2024-08-02 18:30:27 -0400

    Basic layout created

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,7 +4,8 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    let file = FileMiddleware(publicDirectory: app.directory.publicDirectory)
+    app.middleware.use(file)
 
     app.views.use(.leaf)
 
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+        try await req.view.render("index", ["title": "Get Routines"])
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+   app.get("about") { req async throws in
+       try await req.view.render("about", ["title": "About Routines"])
+   }
 }
